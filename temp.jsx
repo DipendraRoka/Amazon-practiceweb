b1964fdfb26035965c38012abf99681d1dce9347
Dipendra Roka <dipendraroka1998@gmail.com>
2026-08-02 16:43:18 +0000

    "hthis is  a  button setter"

---
+++ temp.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to React!</h1>
+      <p>You clicked the button {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
